6e955c3c3b9f8375405350068e37b6750fe4367d
Thomas Stock <thomas@stackhouse.be>
2023-08-24 23:43:20 +0200

    fix zoom tap

--- src/lib/NoteCanvas.svelte
+++ src/lib/NoteCanvas.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <svg
-	class="h-96 max-h-[20vh] w-48 max-w-[50vw] select-none overflow-hidden bg-slate-50 bg-gradient-to-b text-sm font-light shadow hover:cursor-pointer ${$$restProps.class}} m-4 p-2"
+	class="h-96 max-h-[20vh] w-48 max-w-[50vw] touch-manipulation select-none overflow-hidden bg-slate-50 bg-gradient-to-b text-sm font-light shadow hover:cursor-pointer ${$$restProps.class}} m-4 p-2"
 	on:click
 >
 	<foreignObject class="pointer-events-none h-[150%] w-[150%] scale-75" width="150%" height="150%">
