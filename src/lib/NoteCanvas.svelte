441a085ccfd2c119ef80e2cb24dfc14479e3367b
Thomas Stock <thomas@stackhouse.be>
2023-08-24 23:40:29 +0200

    safari fix

--- src/lib/NoteCanvas.svelte
+++ src/lib/NoteCanvas.svelte
@@ -6,7 +6,7 @@
 	class="h-96 max-h-[20vh] w-48 max-w-[50vw] select-none overflow-hidden bg-slate-50 bg-gradient-to-b text-sm font-light shadow hover:cursor-pointer ${$$restProps.class}} m-4 p-2"
 	on:click
 >
-	<foreignObject class="pointer-events-none h-[150%] w-[150%] scale-75">
+	<foreignObject class="pointer-events-none h-[150%] w-[150%] scale-75" width="150%" height="150%">
 		{@html html}
 	</foreignObject>
 </svg>
