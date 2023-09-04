c45d517bd2422df70e18a5baac087baf6db235a6
Thomas Stock <thomas@stackhouse.be>
2023-09-04 20:18:35 +0200

    undo redo progress

--- client/src/store/actions.ts
+++ client/src/store/actions.ts
@@ -2,14 +2,21 @@ import { derived, writable } from 'svelte/store';
 
 export const actions = writable<{ [key: string]: any }[]>([]);
 
-export const canRedo = derived(actions, ($actions) => false);
+export const backs = writable(0);
 
-export const canUndo = derived(actions, ($actions) => Boolean($actions.length));
+export const canRedo = derived([backs], ([$backs]) => $backs > 0);
+
+export const canUndo = derived(
+	[actions, backs],
+	([$actions, $backs]) => $actions.length - $backs > 0
+);
 
 export const undo = () => {
 	console.log('undoing');
+	backs.update((_) => ++_);
 };
 
 export const redo = () => {
 	console.log('redoing');
+	backs.update((_) => --_);
 };
